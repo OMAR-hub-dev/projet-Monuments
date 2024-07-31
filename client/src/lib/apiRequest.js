d42cb6ee5bfb4c22281874b043876197e051b8ab
Oulaya omar <oulaya.omar@gmail.com>
2024-08-01 01:54:08 +0200

    test3 front

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-    baseURL: 'http://localhost:8800/api',
-    // baseURL: 'https://projet-monuments.onrender.com/api',
+    // baseURL: 'http://localhost:8800/api',
+    baseURL: 'https://projet-monuments.onrender.com/api',
     withCredentials: true,
 })
 

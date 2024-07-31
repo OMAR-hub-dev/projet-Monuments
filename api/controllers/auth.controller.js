0c96a2bc2e67667127ff738f95c6b5489e7f0578
Oulaya omar <oulaya.omar@gmail.com>
2024-08-01 01:12:24 +0200

    test 2

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -70,7 +70,7 @@ export const login = async (req, res) => {
         res
             .cookie("token", token, {
                 httpOnly: true,
-                // secure:true,
+                secure:true,
                 maxAge: age,
             })
             .status(200)
